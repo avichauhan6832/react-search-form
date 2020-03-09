501b075c715131a1f6a67619f70cb61526cbcff5
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2020-03-09 19:05:22 +0530

    unit test for searchlist

--- src/components/SearchList/SearchList.test.js
+++ src/components/SearchList/SearchList.test.js
@@ -3,23 +3,34 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import SearchList from './searchList';
+import axios from '../../axios-form';
+import UserCard from '../UserCard/userCard';
 
 configure({adapter: new Adapter()});
 
 describe('<SearchList />', () => {
 
-    let wrapper, users;
+    let wrapper;
+    let users;
 
-    beforeAll(() => {
-        
+    beforeAll(async () => {
+        await axios.get('/search/?input=test').then((response) => {
+            // console.log(response)
+            users = response.data;
+        }).catch((err) => {
+            console.log("*************************")
+            console.log(err);
+        })
     })
 
     beforeEach(() => {
-        wrapper = shallow(<Logo />)
+        wrapper = shallow(<SearchList users={[]} />);
     })
 
 
-    it('should render one <img> tag', () => {
-        expect(wrapper.find('img')).toHaveLength(1);
+    it('should render one or more <UserCard> tag', () => {
+        wrapper.setProps({users: users})
+        // console.log(wrapper.find(UserCard));
+        expect(wrapper.find(UserCard)).toHaveLength(1)
     })
 })
